8ad93368e49f2684df2ef4eecddcc52dfd931983
GiantDwarfRen <114264732+GiantDwarfRen@users.noreply.github.com>
2023-03-02 14:20:51 -0800

    implement if statement for javac and java

--- grade.sh
+++ grade.sh
@@ -2,6 +2,7 @@ CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf *.class
+rm -rf *.txt
 rm -rf ListExamples.java
 
 git clone $1 student-submission
@@ -21,8 +22,21 @@ else
     exit
 fi
 
+javac -cp $CPATH *.java 2>compile-err.txt
+if [[ $? -ne 0 ]]
+then
+    echo 'Compile Failed'
+    cat compile-err.txt
+    exit
+fi
 
 
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > exec-err.txt
+if [[ $? -ne 0 ]]
+then
+    echo 'Test execution failed'
+    cat exec-err.txt
+    exit
+fi
+
 
